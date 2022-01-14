24c0f5746e27f707b1e8f5d9b34c418bc7d0c7e9
Miguel Aguila <maguila.apple@gmail.com>
2022-01-14 10:26:29 +0800

    Completed: 1. exit button for single player page 2. game buttons for single player page

--- iOS_Training_Product_Demo/iOS_Training_Product_Demo/Controller/SinglePlayerVC.swift
+++ iOS_Training_Product_Demo/iOS_Training_Product_Demo/Controller/SinglePlayerVC.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class SinglePlayerVC: UIViewController {
     let vcName: String = "SinglePlayerVC"
+    @IBOutlet weak var rockButton: UIButton!
+    @IBOutlet weak var paperButton: UIButton!
+    @IBOutlet weak var scissorsButton: UIButton!
+    @IBOutlet weak var lizardButton: UIButton!
+    @IBOutlet weak var spockButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        rockButton.setImage(UIImage(named: "rockButton.png"), for: .normal)
+        paperButton.setImage(UIImage(named: "paperButton.png"), for: .normal)
+        scissorsButton.setImage(UIImage(named: "scissorsButton.png"), for: .normal)
+        lizardButton.setImage(UIImage(named: "lizardButton.png"), for: .normal)
+        spockButton.setImage(UIImage(named: "spockButton.png"), for: .normal)
+
         log_VC_Load(vcName: vcName)
     }
     
@@ -21,6 +32,10 @@ class SinglePlayerVC: UIViewController {
     
     override func viewDidDisappear(_ animated: Bool) {
         log_VC_Dismiss(vcName: vcName)
+    }
+    
+    @IBAction func exitButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
 
 }
